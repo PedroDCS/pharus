f6adec715673762f1bb58f73c9209e09bbb77ce0
Pedro Daniel Camargos Soares <48827474+PedroDCS@users.noreply.github.com>
2022-04-13 23:49:08 -0300

    feat: AppColors class create and import fonts

---
+++ lib/app/shared/app_colors/app_colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor20 = const Color(0xFFFDECBB);
+  static Color primaryColor30 = const Color(0xFFFDDCA1);
+  static Color primaryColor50 = const Color(0xFFFCBA7A);
+  static Color primaryColor = const Color(0xFFFDA049);
+  static Color primaryColor80 = const Color(0xFFEA8642);
+  static Color primaryColor100 = const Color(0xFFD3633A);
+
+  static Color secondaryColor60 = const Color(0xFFCFA4F5);
+  static Color secondaryColor80 = const Color(0xFFC082F1);
+  static Color secondaryColor100 = const Color(0xFFA262ED);
+  static Color secondaryColor400 = const Color(0xFF792DE9);
+  static Color secondaryColor500 = const Color(0xFF5C25C6);
+  static Color secondaryColor600 = const Color(0xFF3C20B1);
+
+  static Color tertiaryColor40 = const Color(0xFFF8B9C6);
+  static Color tertiaryColor50 = const Color(0xFFF7A7B8);
+  static Color tertiaryColor70 = const Color(0xFFF387AC);
+  static Color tertiaryColor100 = const Color(0xFFEE4C8E);
+  static Color tertiaryColor300 = const Color(0xFFD53571);
+  static Color tertiaryColor500 = const Color(0xFFC3104A);
+
+  static Color neutralColor10 = const Color(0xFFF1F1F2);
+  static Color neutralColor40 = const Color(0xFFCECED1);
+  static Color neutralColor60 = const Color(0xFFAFAEB3);
+  static Color neutralColor100 = const Color(0xFF787780);
+  static Color neutralColor600 = const Color(0xFF141425);
+  static Color neutralColor800 = const Color(0xFF020305);
+
+  static Color alertColor = const Color(0xFFE5DE00);
+  static Color successColor = const Color(0xFF04B200);
+  static Color errorColor = const Color(0xFF990000);
+}
